b96a67b19eb987c0f8dd3a17845900b2b9443ebf
yook <yhj03069@gmail.com>
2023-07-31 21:58:07 +0900

    Initial Commit

---
+++ CountdownSalary/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CountdownSalary
+//
+//  Created by yook on 2023/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CountdownSalary/CountdownSalaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownSalaryApp.swift
+//  CountdownSalary
+//
+//  Created by yook on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct CountdownSalaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
