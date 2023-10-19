e8ae34b6a4bef3d733f0b1b9dbbac09972c64dd7
yook <yhj03069@gmail.com>
2023-10-19 20:36:20 +0900

    add widget target and upgrade to iOS 17.0

---
+++ widgetAdd/widgetAddBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetAddBundle.swift
+//  widgetAdd
+//
+//  Created by yook on 10/19/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetAddBundle: WidgetBundle {
+    var body: some Widget {
+        widgetAdd()
+    }
+}
